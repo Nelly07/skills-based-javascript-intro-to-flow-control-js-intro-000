559b785f71a91f2d4e4a4a66a3709e7b49aef373
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 16:39:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
-function basicTeenager(age) {
-  if (age >= 13 && age <= 19) {
+function basicTeenager(teenager) {
+  if (teenager >= 13 && teenager <= 19) {
     return "You are a teenager!"
   }
 }
